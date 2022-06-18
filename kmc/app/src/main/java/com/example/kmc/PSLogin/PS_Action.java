818fdd1a1eaaa2d6d02f6d174d6a1ca6314f8501
NUNNA KIREETI <70832255+kireeti05@users.noreply.github.com>
2022-06-18 14:31:13 +0530

    Made changes to Db details

---
+++ kmc/app/src/main/java/com/example/kmc/PSLogin/PS_Action.java
@@ -0,0 +1,103 @@
+package com.example.kmc.PSLogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.kmc.R;
+import com.example.kmc.login.Collector_Login;
+import com.example.kmc.login.SOLogin;
+import com.example.kmc.login.SPLogin;
+
+public class PS_Action extends AppCompatActivity implements View.OnClickListener {
+
+    public CardView card1,card2,card3,card4, card5;
+    String village;
+    String district;
+    String mandal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ps_action);
+
+        card1 = (CardView) findViewById(R.id.c1);
+        card2 = (CardView) findViewById(R.id.c2);
+        card3 = (CardView) findViewById(R.id.c3);
+        card4 = (CardView) findViewById(R.id.c4);
+        card5 = (CardView) findViewById(R.id.c5);
+
+        card1.setOnClickListener(this);
+        card2.setOnClickListener(this);
+        card3.setOnClickListener(this);
+        card4.setOnClickListener(this);
+        card5.setOnClickListener(this);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String value = extras.getString("village");
+            village=value;
+            mandal=extras.getString("mandal");
+            district=extras.getString("district");
+            //The key argument here must match that used in the other activity
+
+        }else{
+            Log.d("extra", "no");
+        }
+
+    }
+    private boolean isNetworkConnected(){
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+    }
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()){
+            case R.id.c1:
+                i = new Intent(this, PSAddEdit.class);
+                i.putExtra("village",village);
+                i.putExtra("mandal",mandal);
+                i.putExtra("district",district);
+                startActivity(i);
+                break;
+            case R.id.c2:
+                i = new Intent(this, PSAmountToDB.class);
+                i.putExtra("village",village);
+                i.putExtra("mandal",mandal);
+                i.putExtra("district",district);
+                startActivity(i);
+                break;
+            case R.id.c3:
+                i = new Intent(this, PSAmountDBToBen.class);
+                i.putExtra("village",village);
+                i.putExtra("mandal",mandal);
+                i.putExtra("district",district);
+                startActivity(i);
+                break;
+            case R.id.c4:
+                i = new Intent(this, PSGrounding.class);
+                i.putExtra("village",village);
+                i.putExtra("mandal",mandal);
+                i.putExtra("district",district);
+                startActivity(i);
+                break;
+            case R.id.c5:
+                i = new Intent(this, PSMasterReport.class);
+                i.putExtra("village",village);
+                i.putExtra("mandal",mandal);
+                i.putExtra("district",district);
+                startActivity(i);
+                break;
+        }
+
+
+    }
+}
